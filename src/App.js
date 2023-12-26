bb387f19ffdc0c1a8419f43fa078dc9495fca115
mazin abed <mazinabed@mazins-MacBook-Pro.local>
2023-12-26 23:34:15 +0300

    adding more code to app

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function App() {
     // tele.MainButton.show();
       // Optionally, you can send the selected items to the bot immediately
       tele.MainButton.setText('Pay :)').show().onClick(function () {
-        const data = "testing"
+        //const data = {cartItems};
         // JSON.stringify({
         //   username: document.querySelector('input[name=username]').value,
         //   password: document.querySelector('input[name=password]').value,
@@ -55,7 +55,7 @@ function App() {
         //   ei_type: document.getElementsByName('ei_type')[0].value,
         //   download_ei: [].filter.call(document.getElementsByName('download_ei'), (c) => c.checked).map(c => c.value)
         //  });
-        Telegram.WebApp.sendData(data);
+        Telegram.WebApp.sendData(cartItems);
         Telegram.WebApp.close();
     });
   };
