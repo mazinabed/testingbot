9f513d09426c8b873b3417e84ffa543c71b04f26
mazin abed <mazinabed@mazins-MacBook-Pro.local>
2023-12-28 22:25:29 +0300

    adding files

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -7,14 +7,6 @@ const Footer = () => {
     <footer className="w-100 mt-auto bg-secondary p-4">
       <div className="container text-center mb-5">
       <h2>مطعمكم. كوت الزهراء. رقم الهاتف: 0780443255</h2>
-      <h3>تواصل معنا</h3>
-        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-          <FaFacebook />
-        </a>
-        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-          <FaInstagram />
-        </a>
-
         <h4>
       تم انشاء وتطوير هذا البوت من قبل{' '}
           <span
